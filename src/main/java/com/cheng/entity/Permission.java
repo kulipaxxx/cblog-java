b5c60beac9a02b719a3ea4ac78d4c223266dd535
kulipaxxx <1845472368@qq.com>
2022-12-10 17:52:18 +0800

    :lipstick: 完善后端权限管理模块

--- src/main/java/com/cheng/entity/Permission.java
+++ src/main/java/com/cheng/entity/Permission.java
@@ -1,14 +1,15 @@
 package com.cheng.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
+
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author author: cheng
